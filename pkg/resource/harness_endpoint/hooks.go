e78b1bfb90313a243969c4b6d842a87a5d133f51
Raina Doshi <rdoshi@splunk.com>
2026-08-05 11:40:13 -0700

    feat(harness): add controller reconciliation artifacts

---
+++ pkg/resource/harness_endpoint/hooks.go
@@ -0,0 +1,56 @@
+package harness_endpoint
+
+import (
+	"context"
+
+	"github.com/aws-controllers-k8s/bedrockagentcorecontrol-controller/pkg/tags"
+	ackmetrics "github.com/aws-controllers-k8s/runtime/pkg/metrics"
+	ackrtlog "github.com/aws-controllers-k8s/runtime/pkg/runtime/log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	svcsdk "github.com/aws/aws-sdk-go-v2/service/bedrockagentcorecontrol"
+)
+
+var syncTags = func(
+	ctx context.Context,
+	client *svcsdk.Client,
+	metrics *ackmetrics.Metrics,
+	resourceARN string,
+	desiredTags map[string]string,
+	existingTags map[string]string,
+) error {
+	return tags.SyncTags(
+		ctx, client, metrics, resourceARN, desiredTags, existingTags,
+	)
+}
+
+func (rm *resourceManager) getTags(
+	ctx context.Context,
+	resourceARN string,
+) (map[string]*string, error) {
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.getTags")
+	defer func() { exit(nil) }()
+
+	resp, err := rm.sdkapi.ListTagsForResource(ctx, &svcsdk.ListTagsForResourceInput{
+		ResourceArn: &resourceARN,
+	})
+	rm.metrics.RecordAPICall("GET", "ListTagsForResource", err)
+	if err != nil {
+		return nil, err
+	}
+	return aws.StringMap(resp.Tags), nil
+}
+
+func (rm *resourceManager) syncTags(
+	ctx context.Context,
+	desired *resource,
+	latest *resource,
+) error {
+	resourceARN := string(*latest.ko.Status.ACKResourceMetadata.ARN)
+	desiredTags := aws.ToStringMap(desired.ko.Spec.Tags)
+	existingTags := aws.ToStringMap(latest.ko.Spec.Tags)
+	return syncTags(
+		ctx, rm.sdkapi, rm.metrics,
+		resourceARN, desiredTags, existingTags,
+	)
+}
